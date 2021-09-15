a34d481905197c921427ec605f15f17fc2ebc9e3
Keitaro Taylor <keitarotaylor@Keitaros-MBP.lan>
2021-09-15 13:31:26 -0400

    9-15-21

---
+++ 8kyu/multiply.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  let product = a * b
+  return product
+}
