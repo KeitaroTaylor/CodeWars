8f1bd200de86d78189293e621be3122ad5972408
Keitaro Taylor <keitarotaylor@gmail.com>
2021-10-19 22:13:12 -0400

    10-19-21

---
+++ 8kyu/convertToInitials.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+
+
+function abbrevName(name){
+let newArr = name.split(' ')
+let nameOne = newArr[0]
+let nameTwo = newArr[1]
+return `${nameOne[0]}.${nameTwo[0]}`.toUpperCase()
+
+}
---
+++ 8kyu/enoughSpace.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5875b200d520904a04000003/train/javascript
+
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+
+
+function enough(cap, on, wait) {
+  if ((cap - on) > wait) {
+    return 0
+  } else {
+    return wait - (cap - on)
+  }
+}
