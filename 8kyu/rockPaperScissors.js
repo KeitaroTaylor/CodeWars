a0f277e3008c3cd5d204c398654ff7ec30de2ebf
keitarotaylor <keitarotaylor@gmail.com>
2022-01-24 22:43:08 -0500

    1-24-22

--- 8kyu/rockPaperScissors.js
+++ 8kyu/rockPaperScissors.js
@@ -10,8 +10,7 @@
 
 
 
-
-const rps = (p1, p2) => {
+const rps = (p1, p2) => { 
   if ((p1 == 'rock' && p2 == 'rock') || (p1 == 'paper' && p2 == 'paper') || (p1 == 'scissors' && p2 == 'scissors')) {
     return 'Draw!'
   } else if ((p1 == 'rock' && p2 == 'scissors') || (p1 == 'paper' && p2 == 'rock') || (p1 == 'scissors' && p2 == 'paper')) {
